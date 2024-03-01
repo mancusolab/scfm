068e972c5cb19d58f26c7150f78904204820c5ec
Camellia <crui@usc.edu>
2024-03-01 11:52:00 -0800

    start

---
+++ src/scfm/infer.py
@@ -0,0 +1,14 @@
+import jax 
+import jax.numpy as jnp
+import jax.random as rdm
+
+def input(X:arraylike, Y:arraylike):
+    #X is the n by p genotype matrix and Y is the n by k transformed pseudo-bulk gene expression matrix of a focal gene
+    pass
+
+
+def  compute_ELBO():
+    pass
+
+def update_params():
+    pass    
