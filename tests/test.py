a4851f853df219b4db247184d42b7920beed0dc7
Camellia <crui@usc.edu>
2024-04-19 23:35:35 -0700

    correct test

--- tests/test.py
+++ tests/test.py
@@ -1,20 +1,16 @@
+import jax
 import jax.numpy as jnp
-import jax.random as rdm
+from scfm.infer import finemap
+import pandas as pd
 
 
-# Generate genotype matrix X and gene expression matrix Y
-key = rdm.PRNGKey(0)
-key, x_key = rdm.split(key)
-key, y_key = rdm.split(key)
-key, b_key = rdm.split(key)
-N, p, k = 500, 100, 10
-r2 = 0.5
-
-beta = rdm.normal(b_key, shape=(p,))
-X = rdm.normal(x_key, shape=(N, p))
-
-g = X @ beta
-s2g = jnp.var(g)
-s2e = (1 - r2) / r2 * s2g
-
-Y = g + jnp.sqrt(s2e) * rdm.normal(y_key, shape=(N,))
+X_df = pd.read_csv("ENSG00000012983.geno.tsv", sep="\t", header=None)
+Y_df = pd.read_csv("ENSG00000012983_3_2_0.3_0.4_0.05_0.05_2.pheno.tsv", sep="\t", header=None)
+    # Convert pandas DataFrames to NumPy arrays
+Y_np = Y_df.to_numpy()
+X_np = X_df.to_numpy()
+    # Convert NumPy arrays to JAX arrays
+Y = jnp.array(Y_np)
+X = jnp.array(X_np)
+prior, post, pip_all, pip_cs, cs, full_alphas, elbo, elbo_increase, l_order = finemap(Y, X, 1)
+print(cs)
